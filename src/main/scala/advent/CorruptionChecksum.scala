5c973a47bc0104873ee483c081766d9e976c8737
jadilovic <adilovic79@yahoo.com>
2022-11-07 07:04:36 +0100

    Added stories

--- src/main/scala/advent/CorruptionChecksum.scala
+++ src/main/scala/advent/CorruptionChecksum.scala
@@ -9,7 +9,7 @@ object CorruptionChecksum {
     val randomNumbersTable: Seq[List[String]] = Seq(randomNumbersSpreadSheet.split("\n")(0).split("\t").toList, randomNumbersSpreadSheet.split("\n")(1).split("\t").toList, randomNumbersSpreadSheet.split("\n")(2).split("\t").toList, randomNumbersSpreadSheet.split("\n")(3).split("\t").toList, randomNumbersSpreadSheet.split("\n")(4).split("\t").toList, randomNumbersSpreadSheet.split("\n")(5).split("\t").toList, randomNumbersSpreadSheet.split("\n")(6).split("\t").toList, randomNumbersSpreadSheet.split("\n")(7).split("\t").toList, randomNumbersSpreadSheet.split("\n")(8).split("\t").toList, randomNumbersSpreadSheet.split("\n")(9).split("\t").toList, randomNumbersSpreadSheet.split("\n")(10).split("\t").toList, randomNumbersSpreadSheet.split("\n")(11).split("\t").toList, randomNumbersSpreadSheet.split("\n")(12).split("\t").toList, randomNumbersSpreadSheet.split("\n")(13).split("\t").toList, randomNumbersSpreadSheet.split("\n")(14).split("\t").toList, randomNumbersSpreadSheet.split("\n")(15).split("\t").toList)
     val checkSum: Int = randomNumbersTable.map { row => row.map(_.toInt).max - row.map(_.toInt).min }.sum
 
-    println("DAY 2 - Corruption Checksum 1/2")
+    println("DAY 2 - 2. Corruption Checksum 1/2")
     println(s"For each row, determine the difference between the largest value and the smallest value; the checksum is the sum of all of these differences ${checkSum}")
     println("")
 
@@ -21,7 +21,7 @@ object CorruptionChecksum {
       }.filter(item => item.nonEmpty).head
     }.sum
 
-    println("DAY 2 - Corruption Checksum 2/2")
+    println("DAY 2 - 2. Corruption Checksum 2/2")
     println(s"Find the only two numbers in each row where one evenly divides the other and add up each row's result ${sumEvenlyDivisible}")
     println("")
   }
